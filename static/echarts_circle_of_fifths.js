737c8eca2ac8f5117e3b438519a41618ca54775c
Steven Cobb <sjcobb.dev@gmail.com>
2021-05-31 14:39:20 -0400

    circle fifths 2d, debug note order to repos c maj to top

--- static/echarts_circle_of_fifths.js
+++ static/echarts_circle_of_fifths.js
@@ -1,6 +1,14 @@
 ///////////////////
 // ECHARTS GRAPH //
 ///////////////////
+// http://localhost:8082/graph.html
+
+// https://github.com/tonaljs/tonal
+// // import { Note, Interval, Scale } from "@tonaljs/tonal";
+// import * as Tonal from "../node_modules/@tonaljs/tonal/browser/tonal.min.js";
+// console.log(Tonal);
+
+console.log(Tonal.Key.minorKey("Ab"));
 
 // https://echarts.apache.org/en/option.html#series-graph.type
 
@@ -15,8 +23,28 @@ let circleFifthsId = document.getElementById('circle-of-fifths');
 // var graphCircleFifths = echarts.init(circleFifthsId, 'dark-blue');
 var graphCircleFifths = echarts.init(circleFifthsId, 'tech-blue');
 
+// const nodes = [
+//     { name: "C", value: 100, category: 0, id: 0, symbolSize: 60, x: 0, y: 0, },
+//     // { name: "C", value: 100, category: 0, id: 0, symbolSize: 60, x: -1000, y: -1000, },
+//     // { name: "C#", value: 100, category: 0, id: 99, symbolSize: 60, x: 100, y: 100, },
+//     { name: "G", value: 50, category: 0, id: 1, x: 100, y: 100, },
+//     { name: "D", value: 5, category: 0, id: 2, x: 110, y: 90, },
+//     { name: "A", value: 5, category: 0, id: 3 },
+//     { name: "E", value: 5, category: 0, id: 4 },
+//     { name: "B", value: 5, category: 0, id: 5 },
+//     { name: "Gb", value: 5, category: 0, id: 6 },
+//     { name: "Db", value: 5, category: 0, id: 7 },
+//     { name: "Ab", value: 5, category: 0, id: 8 },
+//     { name: "Eb", value: 5, category: 0, id: 9 },
+//     { name: "Bb", value: 5, category: 0, id: 10 },
+//     { name: "F", value: 5, category: 0, id: 11, x: -10, y: 10 },
+
+//     // { name: "Am", value: 30, category: 1, id: 12 },
+//     // { name: "Em", value: 15, category: 1, id: 13 },
+// ];
+
 const nodes = [
-    { name: "C", value: 100, category: 0, id: 0, symbolSize: 60, x: 0, y: 0, },
+    { name: "C", value: 100, category: 0, id: 0 },
     { name: "G", value: 50, category: 0, id: 1 },
     { name: "D", value: 5, category: 0, id: 2 },
     { name: "A", value: 5, category: 0, id: 3 },
@@ -27,7 +55,8 @@ const nodes = [
     { name: "Ab", value: 5, category: 0, id: 8 },
     { name: "Eb", value: 5, category: 0, id: 9 },
     { name: "Bb", value: 5, category: 0, id: 10 },
-    { name: "F", value: 5, category: 0, id: 11, x: -10, y: 10 },
+    { name: "F", value: 5, category: 0, id: 11 },
+    // { name: "C", value: 100, category: 0, id: 12 },
 
     // { name: "Am", value: 30, category: 1, id: 12 },
     // { name: "Em", value: 15, category: 1, id: 13 },
@@ -40,6 +69,7 @@ const categories = [
 ];
 
 const edges = [
+    // { source: 12, target: 1},  // C -> G
     { source: 0, target: 1},  // C -> G
     { source: 1, target: 2},  // G -> D
     { source: 2, target: 3},  // D -> A
@@ -52,6 +82,7 @@ const edges = [
     { source: 9, target: 10}, // Eb -> Bb
     { source: 10, target: 11}, // Bb -> F
     { source: 11, target: 0}, // F -> C
+    // { source: 11, target: 12}, // F -> C
 
     // Minor inner circle
     // { source: 0, target: 12},  // C -> Am
